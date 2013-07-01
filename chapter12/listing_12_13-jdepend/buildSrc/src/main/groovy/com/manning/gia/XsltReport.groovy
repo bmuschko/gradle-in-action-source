63d4fa28a3aec48430c81e733766d5fa8914b0da
Benjamin Muschko <benjamin.muschko@gmail.com>
2013-07-01 18:39:23 -0400

    Source code for code quality management chapter

---
+++ chapter12/listing_12_13-jdepend/buildSrc/src/main/groovy/com/manning/gia/XsltReport.groovy
@@ -0,0 +1,30 @@
+package com.manning.gia
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.Optional
+
+class XsltReport extends DefaultTask {
+	@InputFile File inputFile
+	@InputFile File xslStyleFile
+	@Input @Optional Map<String, String> params = [:]
+	@OutputFile File outputFile
+	
+	XsltReport() {
+		onlyIf {
+			inputFile.exists()
+		}		
+	} 
+
+	@TaskAction
+	void start() {
+		ant.xslt(in: inputFile, style: xslStyleFile, out: outputFile) {
+			params.each { key, value ->
+				ant.param(name: key, expression: value)
+			}
+		}
+	}
+}
