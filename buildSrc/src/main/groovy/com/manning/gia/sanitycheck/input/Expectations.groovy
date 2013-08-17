4343571fe86bfc50f4aee860945163704b6d66c7
Benjamin Muschko <benjamin.muschko@gmail.com>
2013-08-17 11:44:42 -0500

    Add missing class

---
+++ buildSrc/src/main/groovy/com/manning/gia/sanitycheck/input/Expectations.groovy
@@ -0,0 +1,33 @@
+package com.manning.gia.sanitycheck.input
+
+class Expectations {
+    Result result = Result.SUCCESS
+
+    static enum Result {
+        SUCCESS('success'), FAILURE('failure')
+        
+        private static final Map RESULTS
+
+        static {
+            RESULTS = [:]
+
+            values().each { result ->
+                RESULTS.put(result.name, result)
+            }
+        }
+
+        private final name
+
+        private Result(name) {
+            this.name = name
+        }
+        
+        static Result getResultForName(String name) {
+            if(!RESULTS.containsKey(name)) {
+                throw new IllegalArgumentException("Unsupported result expectation: $name")
+            }
+            
+            RESULTS[name]
+        }
+    }
+}
