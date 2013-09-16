8906a99ff0018bb963ef469f5cb4096a62be8107
Benjamin Muschko <benjamin.muschko@gmail.com>
2013-09-16 07:18:33 -0400

    Renamed

--- chapter09/migrating-ant-build/ant/src/main/java/com/mycompany/app/Main.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.app;
-
-import org.apache.commons.lang3.StringUtils;
-
-public class Main {
-   public static void main(String[] args) {
-      System.out.println(StringUtils.capitalize("hello"));
-   }
-}
