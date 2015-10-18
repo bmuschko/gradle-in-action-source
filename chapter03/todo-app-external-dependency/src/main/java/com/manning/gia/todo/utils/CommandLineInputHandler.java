0cc892d158f5ffff20ec156c78353192280b4815
Benjamin Muschko <benjamin.muschko@gmail.com>
2015-10-18 08:47:13 -0400

    Removed unnecessary System.out.println.

--- chapter03/todo-app-external-dependency/src/main/java/com/manning/gia/todo/utils/CommandLineInputHandler.java
+++ chapter03/todo-app-external-dependency/src/main/java/com/manning/gia/todo/utils/CommandLineInputHandler.java
@@ -22,8 +22,6 @@ public class CommandLineInputHandler {
 
     public String readInput() {
         java.io.Console console = System.console();
-        System.out.println("-----> console: " + console);
-        System.out.println("-----> console2 : " + new java.util.Scanner(System.in));
         return new java.util.Scanner(System.in).useDelimiter(System.getProperty("line.separator")).next();
     }
 
